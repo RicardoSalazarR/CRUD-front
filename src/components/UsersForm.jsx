4ef44c2aa044a39ed54e6ca3e83caca66ab979a9
Katia Jauregui <katiajauregui@gmail.com>
2022-11-12 20:51:05 -0600

    input styles

--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -35,26 +35,29 @@ const UsersForm = ({ postUser, selectedUser, putUser }) => {
             className='usersForm'
             onSubmit={handleSubmit(submit)}
         >
-            <div>
-                <label htmlFor="name">Name: </label>
-                <input {...register('first_name')} type="text" id='name' />
+            <div className='form-inputs'>
+                <div className='label-input'>
+                    <label htmlFor="name">Name: </label>
+                    <input {...register('first_name')} type="text" id='name' />
+                </div>
+                <div className='label-input'>
+                    <label htmlFor="last-name">Last Name: </label>
+                    <input {...register('last_name')} type="text" id='last-name' />
+                </div>
+                <div className='label-input'>
+                    <label htmlFor="password">Password: </label>
+                    <input {...register('password')} type="password" id='password' />
+                </div>
+                <div className='label-input'>
+                    <label htmlFor="email">Email: </label>
+                    <input {...register('email')} type="email" id='email' />
+                </div>
+                <div className='label-input'>
+                    <label htmlFor="birthday">Birthday: </label>
+                    <input {...register('birthday')} type="date" id='birthday' />
+                </div>
             </div>
-            <div>
-                <label htmlFor="last-name">Last Name: </label>
-                <input {...register('last_name')} type="text" id='last-name' />
-            </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input {...register('password')} type="password" id='password' />
-            </div>
-            <div>
-                <label htmlFor="email">email: </label>
-                <input {...register('email')} type="email" id='email' />
-            </div>
-            <div>
-                <label htmlFor="birthday">Birthday: </label>
-                <input {...register('birthday')} type="date" id='birthday' />
-            </div>
+
             <button className='btn-submit'>Submit</button>
         </form>
     );
